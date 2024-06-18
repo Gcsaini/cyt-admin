4d05bcf23882ac673cd32f763cb19de2a0ca8ff6
Gopi Chand <gopi.chand@aajtak.com>
2024-06-18 17:00:07 +0530

    updated path

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,5 +1,5 @@
 const localhost = "http://localhost:4000/api";
-const apiUrl = "http://cytapi.ap-south-1.elasticbeanstalk.com/api";
+const apiUrl = "https://cytapi.ap-south-1.elasticbeanstalk.com/api";
 export const loginUrl = `${localhost}/auth/login`;
 export const getTherapists = `${apiUrl}/get-therapists`;
 export const sendAproveMail = `${apiUrl}/send-aprove-mail`;
