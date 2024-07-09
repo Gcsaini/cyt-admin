18b3ca88827d36c65dab38fe771ad65d782c689d
Sooraj Singh <soorajsingh7505@gmail.com>
2024-07-10 03:18:49 +0530

    upload

--- src/components/header.js
+++ src/components/header.js
@@ -1,147 +1,147 @@
-import LogoImg from "../img/logo.png";
+import LogoImg from "../../src/assets/img/logo.png";
 import DoctorThumb from "../img/doctors/doctor-thumb-01.jpg";
 import PatientImg from "../img/patients/patient1.jpg";
 import Avatar from "../img/profiles/avatar-01.jpg";
 import LazyImage from "../helpers/lazy-image";
+import "../../src/assets/css/custom.css";
 export default function Header() {
   return (
-    <div className="header">
-      <div className="header-left">
-        <a href="index.html" className="logo">
-          <LazyImage src={LogoImg} alt="Logo" />
+    <div class="header">
+      <div class="header-left">
+        <a href="index.html" class="logo">
+          <img src={LogoImg} alt="Logo" />
         </a>
-        <a href="index.html" className="logo logo-small">
-          <LazyImage src={LogoImg} alt="Logo" width="30" height="30" />
+        <a href="index.html" class="logo logo-small">
+          <img
+            src="assets/img/logo-small.png"
+            alt="Logo"
+            width="30"
+            height="30"
+          />
         </a>
       </div>
 
-      <a href="/toggle" id="toggle_btn">
-        <i className="fe fe-text-align-left"></i>
+      <a href="javascript:void(0);" id="toggle_btn">
+        <i class="fe fe-text-align-left"></i>
       </a>
-      <div className="top-nav-search">
+      <div class="top-nav-search">
         <form>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search here"
-          />
-          <button className="btn" type="submit">
-            <i className="fa fa-search"></i>
+          <input type="text" class="form-control" placeholder="Search here" />
+          <button class="btn" type="submit">
+            <i class="fa fa-search"></i>
           </button>
         </form>
       </div>
 
-      <a className="mobile_btn" id="mobile_btn" href="/mobile">
-        <i className="fa fa-bars"></i>
+      <a class="mobile_btn" id="mobile_btn">
+        <i class="fa fa-bars"></i>
       </a>
 
-      <ul className="nav user-menu">
-        <li className="nav-item dropdown noti-dropdown">
+      <ul class="nav user-menu">
+        <li class="nav-item dropdown noti-dropdown">
           <a
-            href="/dropdown"
-            className="dropdown-toggle nav-link"
+            href="#"
+            class="dropdown-toggle nav-link"
             data-bs-toggle="dropdown"
           >
-            <i className="fe fe-bell"></i>{" "}
-            <span className="badge rounded-pill">3</span>
+            <i class="fe fe-bell"></i> <span class="badge rounded-pill">3</span>
           </a>
-          <div className="dropdown-menu notifications">
-            <div className="topnav-dropdown-header">
-              <span className="notification-title">Notifications</span>
-              <a href="/clear-all" className="clear-noti">
+          <div class="dropdown-menu notifications">
+            <div class="topnav-dropdown-header">
+              <span class="notification-title">Notifications</span>
+              <a href="javascript:void(0)" class="clear-noti">
                 {" "}
                 Clear All{" "}
               </a>
             </div>
-            <div className="noti-content">
-              <ul className="notification-list">
-                <li className="notification-message">
-                  <a href="/profile">
-                    <div className="notify-block d-flex">
-                      <span className="avatar avatar-sm flex-shrink-0">
-                        <LazyImage
-                          className="avatar-img rounded-circle"
+            <div class="noti-content">
+              <ul class="notification-list">
+                <li class="notification-message">
+                  <a href="#">
+                    <div class="notify-block d-flex">
+                      <span class="avatar avatar-sm flex-shrink-0">
+                        <img
+                          class="avatar-img rounded-circle"
                           alt="User Image"
-                          src={DoctorThumb}
+                          src="assets/img/doctors/doctor-thumb-01.jpg"
                         />
                       </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Dr. Ruby Perrin</span>{" "}
+                      <div class="media-body flex-grow-1">
+                        <p class="noti-details">
+                          <span class="noti-title">Dr. Ruby Perrin</span>{" "}
                           Schedule{" "}
-                          <span className="noti-title">her appointment</span>
+                          <span class="noti-title">her appointment</span>
                         </p>
-                        <p className="noti-time">
-                          <span className="notification-time">4 mins ago</span>
+                        <p class="noti-time">
+                          <span class="notification-time">4 mins ago</span>
                         </p>
                       </div>
                     </div>
                   </a>
                 </li>
-                <li className="notification-message">
-                  <a href="/profile">
-                    <div className="notify-block d-flex">
-                      <span className="avatar avatar-sm flex-shrink-0">
-                        <LazyImage
-                          className="avatar-img rounded-circle"
+                <li class="notification-message">
+                  <a href="#">
+                    <div class="notify-block d-flex">
+                      <span class="avatar avatar-sm flex-shrink-0">
+                        <img
+                          class="avatar-img rounded-circle"
                           alt="User Image"
-                          src={PatientImg}
+                          src="assets/img/patients/patient1.jpg"
                         />
                       </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Charlene Reed</span> has
+                      <div class="media-body flex-grow-1">
+                        <p class="noti-details">
+                          <span class="noti-title">Charlene Reed</span> has
                           booked her appointment to{" "}
-                          <span className="noti-title">Dr. Ruby Perrin</span>
+                          <span class="noti-title">Dr. Ruby Perrin</span>
                         </p>
-                        <p className="noti-time">
-                          <span className="notification-time">6 mins ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li className="notification-message">
-                  <a href="/profile">
-                    <div className="notify-block d-flex">
-                      <span className="avatar avatar-sm flex-shrink-0">
-                        <LazyImage
-                          className="avatar-img rounded-circle"
-                          alt="User Image"
-                          src={PatientImg}
-                        />
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Travis Trimble</span>{" "}
-                          sent a amount of $210 for his{" "}
-                          <span className="noti-title">appointment</span>
-                        </p>
-                        <p className="noti-time">
-                          <span className="notification-time">8 mins ago</span>
+                        <p class="noti-time">
+                          <span class="notification-time">6 mins ago</span>
                         </p>
                       </div>
                     </div>
                   </a>
                 </li>
-                <li className="notification-message">
-                  <a href="/profile">
-                    <div className="notify-block d-flex">
-                      <span className="avatar avatar-sm flex-shrink-0">
-                        <LazyImage
-                          className="avatar-img rounded-circle"
+                <li class="notification-message">
+                  <a href="#">
+                    <div class="notify-block d-flex">
+                      <span class="avatar avatar-sm flex-shrink-0">
+                        <img
+                          class="avatar-img rounded-circle"
                           alt="User Image"
-                          src={PatientImg}
+                          src="assets/img/patients/patient2.jpg"
                         />
                       </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Carl Kelly</span> send a
-                          message{" "}
-                          <span className="noti-title"> to his doctor</span>
+                      <div class="media-body flex-grow-1">
+                        <p class="noti-details">
+                          <span class="noti-title">Travis Trimble</span> sent a
+                          amount of $210 for his{" "}
+                          <span class="noti-title">appointment</span>
                         </p>
-                        <p className="noti-time">
-                          <span className="notification-time">12 mins ago</span>
+                        <p class="noti-time">
+                          <span class="notification-time">8 mins ago</span>
+                        </p>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li class="notification-message">
+                  <a href="#">
+                    <div class="notify-block d-flex">
+                      <span class="avatar avatar-sm flex-shrink-0">
+                        <img
+                          class="avatar-img rounded-circle"
+                          alt="User Image"
+                          src="assets/img/patients/patient3.jpg"
+                        />
+                      </span>
+                      <div class="media-body flex-grow-1">
+                        <p class="noti-details">
+                          <span class="noti-title">Carl Kelly</span> send a
+                          message <span class="noti-title"> to his doctor</span>
+                        </p>
+                        <p class="noti-time">
+                          <span class="notification-time">12 mins ago</span>
                         </p>
                       </div>
                     </div>
@@ -149,48 +149,48 @@ export default function Header() {
                 </li>
               </ul>
             </div>
-            <div className="topnav-dropdown-footer">
-              <a href="/view-all">View all Notifications</a>
+            <div class="topnav-dropdown-footer">
+              <a href="#">View all Notifications</a>
             </div>
           </div>
         </li>
 
-        <li className="nav-item dropdown has-arrow">
+        <li class="nav-item dropdown has-arrow">
           <a
-            href="/profile"
-            className="dropdown-toggle nav-link"
+            href="#"
+            class="dropdown-toggle nav-link"
             data-bs-toggle="dropdown"
           >
-            <span className="user-img">
-              <LazyImage
-                className="rounded-circle"
-                src={Avatar}
+            <span class="user-img">
+              <img
+                class="rounded-circle"
+                src="assets/img/profiles/avatar-01.jpg"
                 width="31"
                 alt="Ryan Taylor"
               />
             </span>
           </a>
-          <div className="dropdown-menu">
-            <div className="user-header">
-              <div className="avatar avatar-sm">
-                <LazyImage
-                  src={Avatar}
+          <div class="dropdown-menu">
+            <div class="user-header">
+              <div class="avatar avatar-sm">
+                <img
+                  src="assets/img/profiles/avatar-01.jpg"
                   alt="User Image"
-                  className="avatar-img rounded-circle"
+                  class="avatar-img rounded-circle"
                 />
               </div>
-              <div className="user-text">
+              <div class="user-text">
                 <h6>Ryan Taylor</h6>
-                <p className="text-muted mb-0">Administrator</p>
+                <p class="text-muted mb-0">Administrator</p>
               </div>
             </div>
-            <a className="dropdown-item" href="profile.html">
+            <a class="dropdown-item" href="profile.html">
               My Profile
             </a>
-            <a className="dropdown-item" href="settings.html">
+            <a class="dropdown-item" href="settings.html">
               Settings
             </a>
-            <a className="dropdown-item" href="login.html">
+            <a class="dropdown-item" href="login.html">
               Logout
             </a>
           </div>
--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,76 +1,71 @@
 import { useNavigate } from "react-router-dom";
+import "../../src/assets/css/custom.css";
+import {Scriptfile} from "../../src/assets/js/script.js";
+
 export default function SideNav() {
   const navigate = useNavigate();
-  function handleClick(nav) {
+
+  function handleDashboard(nav) {
     navigate(nav);
   }
   return (
-    <div className="sidebar" id="sidebar">
-      <div className="sidebar-inner slimscroll">
-        <div id="sidebar-menu" className="sidebar-menu">
-          <ul>
-            <li className="menu-title">
-              <span>Main</span>
-            </li>
-            <li className="active">
-              <a href="index.html">
-                <i className="fe fe-home"></i> <span>Dashboard</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                onClick={() => {
-                  handleClick("/registrations");
-                }}
-              >
-                <i className="fe fe-layout"></i> <span>Registrations</span>
-              </a>
-            </li>
-            <li>
-              <a href="specialities.html">
-                <i className="fe fe-users"></i> <span>Specialities</span>
-              </a>
-            </li>
-            <li>
-              <a href="doctor-list.html">
-                <i className="fe fe-user-plus"></i> <span>Doctors</span>
-              </a>
-            </li>
-            <li>
-              <a href="patient-list.html">
-                <i className="fe fe-user"></i> <span>Patients</span>
-              </a>
-            </li>
-            <li>
-              <a href="reviews.html">
-                <i className="fe fe-star-o"></i> <span>Reviews</span>
-              </a>
-            </li>
-            <li>
-              <a href="transactions-list.html">
-                <i className="fe fe-activity"></i> <span>Transactions</span>
-              </a>
-            </li>
-            <li>
-              <a href="settings.html">
-                <i className="fe fe-vector"></i> <span>Settings</span>
-              </a>
-            </li>
-            <li className="submenu">
-              <a href="reports">
-                <i className="fe fe-document"></i> <span> Reports</span>{" "}
-                <span className="menu-arrow"></span>
-              </a>
-              <ul style={{ display: "none" }}>
-                <li>
-                  <a href="invoice-report.html">Invoice Reports</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
+    <>
+      <div className="sidebar" id="sidebar">
+        <div className="sidebar-inner slimscroll">
+          <div id="sidebar-menu" className="sidebar-menu">
+            <ul>
+              <li className="menu-title">
+                <span>Main</span>
+              </li>
+              <li className="active">
+                <a href="#" onClick={() => handleDashboard("/home")}>
+                  <i className="fe fe-home"></i> <span>Dashboard</span>
+                </a>
+              </li>
+              <li className="submenu">
+                <a href="#">
+                  <i className="fe fe-layout"></i> <span> Registration</span>{" "}
+                  <span className="menu-arrow"></span>
+                </a>
+                <ul style={{ display: "none" }}>
+                  <li>
+                    <a href="#">Client</a>
+                  </li>
+                  <li>
+                    <a href="#">Therapist</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="specialities.html">
+                  <i className="fe fe-users"></i> <span>Therapists</span>
+                </a>
+              </li>
+              <li>
+                <a href="doctor-list.html">
+                  <i className="fe fe-user-plus"></i> <span>Clients</span>
+                </a>
+              </li>
+              <li>
+                <a href="patient-list.html">
+                  <i className="fe fe-user"></i> <span>Appointments</span>
+                </a>
+              </li>
+              <li>
+                <a href="reviews.html">
+                  <i className="fe fe-star-o"></i> <span>Reviews</span>
+                </a>
+              </li>
+              <li>
+                <a href="transactions-list.html">
+                  <i className="fe fe-activity"></i> <span>Invoices</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+      <script src={Scriptfile}></script>
+    </>
   );
 }
