09e491443bdb6ffc670f8f26b9d041610c0bc572
Gopi Chand <gopi.chand@aajtak.com>
2024-09-25 18:57:54 +0530

    aaded api url

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,6 +1,13 @@
-const localhost = "http://localhost:4000/api";
-const apiUrl = "https://cytapi.ap-south-1.elasticbeanstalk.com/api";
-export const loginUrl = `${localhost}/auth/login`;
+let apiUrl;
+const currentDomain = window.location.hostname;
+
+if (currentDomain === "localhost") {
+  apiUrl = "http://localhost:4000/api";
+} else {
+  apiUrl = "https://cyt-backend.onrender.com/api";
+}
+
+export const loginUrl = `${apiUrl}/auth/login`;
 export const getTherapists = `${apiUrl}/get-therapists`;
 export const sendAproveMail = `${apiUrl}/send-aprove-mail`;
 export const aprovedTherapist = `${apiUrl}/aprove-therapist`;
